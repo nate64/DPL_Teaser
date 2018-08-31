4a1dc6f428759f17a132fd2472b31f1c47695bd3
Nate <nathanestes@gmail.com>
2018-08-31 09:46:13 -0600

    added prime

---
+++ isPrime.rb
@@ -0,0 +1,10 @@
+# Write a script that accepts a number as a parameter 
+# and check the number is prime or not.
+# Note : A prime number (or a prime) is a natural number 
+# greater than 1 that has no positive divisors other than 1 and itself.
+
+
+
+isPrime
+
+take in number - divide by itself and 1
